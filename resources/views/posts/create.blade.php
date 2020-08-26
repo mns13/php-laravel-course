6b3e960e9b9719db7d9ea31e26eac5ae97f70f6a
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-26 18:13:33 +0300

    install laravel/illuminate 6.0  package and use it in create form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,9 +2,13 @@
 @section('title', 'Create Post')
 @section('content')
     <h1>Create a Post</h1>
-    <form action="/posts" method="post">
+{{--    <form action="/posts" method="post">--}}
+
+    {!! Form::open(['url'=>'/posts']) !!}
         @csrf
         <input type="text" name="title">
         <button type="submit" name="submit">submit</button>
-    </form>
+    {!! Form::close() !!}
+
+{{--    </form>--}}
 @section('footer')
